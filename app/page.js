270f3855ade6f22905c8875010c077f793b34e1d
bdshakhawat <mdshakwat.hossain@gmail.com>
2024-04-28 09:42:07 +0600

    first commit

---
+++ app/page.js
@@ -0,0 +1,13 @@
+import Hero from './components/Hero';
+import Layout from './components/Layout';
+import Footer from './components/Footer';
+
+export default function Home() {
+	return (
+		<>
+			<Hero />
+			<Layout />
+			<Footer />
+		</>
+	);
+}
---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+	images: {
+		domains: ['solamente.store'],
+	},
+};
+
+module.exports = nextConfig
